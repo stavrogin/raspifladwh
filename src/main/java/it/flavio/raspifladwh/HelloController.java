b276ab83fa78b121e9582ca6aa901bfad9844bba
Flavio Ricci <flavio.ricci@vidiemme.it>
2019-10-17 09:36:52 +0200

    :tada: setup skeleton with spring boot

---
+++ src/main/java/it/flavio/raspifladwh/HelloController.java
@@ -0,0 +1,14 @@
+package it.flavio.raspifladwh;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+	
+}
---
+++ src/test/java/it/flavio/raspifladwh/AppTest.java
@@ -0,0 +1,15 @@
+package it.flavio.raspifladwh;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void testOfTest() {
+		assertThat(true);
+	}
+}
